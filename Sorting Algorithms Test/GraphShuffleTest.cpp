41d258e0eab69231c8a444c677d55aff29d697ea
Anthony Pham <anthonyphamdd@gmail.com>
2024-09-04 20:41:13 -0700

    Finished unit testing graph shuffle function

--- Sorting Algorithms Test/GraphShuffleTest.cpp
+++ Sorting Algorithms Test/GraphShuffleTest.cpp
@@ -11,21 +11,20 @@ bool vectorCompare(const sf::RectangleShape& rect1, const sf::RectangleShape& re
     (rect1.getOutlineColor() == rect2.getOutlineColor()) &&
     (rect1.getOutlineThickness() == rect2.getOutlineThickness());
 }
-// documentation
+/* documentation
 TEST(TestCaseName, TestName) {
   ASSERT_EQ(1, 1); // fatal equals
   EXPECT_EQ(1, 1); // non-fatal equals
   EXPECT_NE(2, 1); // non-fatal not equals
   EXPECT_TRUE(true);
-  /* output upon non-fatal errors
+  // output upon non-fatal errors
   for (int i = 0; i < x.size(); ++i) {
     EXPECT_EQ(x[i], y[i]) << "Vectors x and y differ at index " << i;
   }
-  */  
-}
+} */
 
 // SEH exception solved with https://stackoverflow.com/questions/13157671/seh-exception-with-code-0xc0000005-thrown-in-the-test-body
-TEST(GraphShuffleTest, ShuffleEmptyVector) {
+TEST(GraphShuffleTest, Shuffle0Vector) {
   Graph graph(0);
   std::vector<sf::RectangleShape> dupe = graph.m_histogram;
   graph.shuffle();
@@ -37,14 +36,41 @@ TEST(GraphShuffleTest, ShuffleEmptyVector) {
   EXPECT_TRUE(std::equal(graph.m_histogram.begin(), graph.m_histogram.end(), dupe.begin(), vectorCompare));
 }
 
-TEST(GraphShuffleTest, ShuffleOneVector) {
+TEST(GraphShuffleTest, Shuffle1Vector) {
   Graph graph(1);
   std::vector<sf::RectangleShape> dupe = graph.m_histogram;
   graph.shuffle();
-  // this is essentially the same thing as std::equal
-  /*for (int i = 0; i < graph.m_histogram.size(); ++i) {
-    EXPECT_TRUE(vectorCompare(graph.m_histogram.at(i), dupe.at(i)));
-  }
-  */
   EXPECT_TRUE(std::equal(graph.m_histogram.begin(), graph.m_histogram.end(), dupe.begin(), vectorCompare));
+}
+
+TEST(GraphShuffleTest, Shuffle2Vector) {
+  Graph graph(2);
+  int count = 0;
+  int countMax = 150;
+  std::vector<sf::RectangleShape> dupe = graph.m_histogram;
+  graph.shuffle();
+  while (std::equal(graph.m_histogram.begin(), graph.m_histogram.end(), dupe.begin(), vectorCompare)
+         && count < countMax) {
+    graph.shuffle();
+    ++count;
+  }
+
+  EXPECT_FALSE(std::equal(graph.m_histogram.begin(), graph.m_histogram.end(), dupe.begin(), vectorCompare));
+  EXPECT_TRUE(count < countMax);
+}
+
+TEST(GraphShuffleTest, Shuffle50Vector) {
+  Graph graph(50);
+  int count = 0;
+  int countMax = 150;
+  std::vector<sf::RectangleShape> dupe = graph.m_histogram;
+  graph.shuffle();
+  while (std::equal(graph.m_histogram.begin(), graph.m_histogram.end(), dupe.begin(), vectorCompare)
+         && count < countMax) {
+    graph.shuffle();
+    ++count;
+  }
+
+  EXPECT_FALSE(std::equal(graph.m_histogram.begin(), graph.m_histogram.end(), dupe.begin(), vectorCompare));
+  EXPECT_TRUE(count < countMax);
 }
