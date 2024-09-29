a76b1d7bcc019be7c0d6fd76a38dd1f3845fdfd7
Anthony Pham <anthonyphamdd@gmail.com>
2024-09-29 10:27:39 -0700

    refactored imgui into a separate class

--- Sorting Algorithms/DrawImgui.h
+++ Sorting Algorithms/DrawImgui.h
@@ -7,26 +7,39 @@
 #include "LinkedList.h"
 #include "imgui.h"
 #include "imgui-SFML.h"
+#include <iostream>
+using namespace std;
 
 class DrawImgui {
 public:
-  DrawImgui(sf::RenderWindow& win, Algorithms& alg, Graph& g, LinkedList& LL) : 
+  DrawImgui(sf::RenderWindow& win, Algorithms& alg, Graph& g, LinkedList& LL, Control& con, sf::View& vw) : 
     window(win),
     algo(alg),
     graph(g),
-    linkedList(LL) {
+    linkedList(LL),
+    control(con), 
+    view(vw) {
   };
 
-  void run();
   void dockspace();
+  void run();
+  void sortingAlgsTab();
+  void linkedListTab();
+  void binaryTreeTab();
+  void graphTab();
+  void mazeTab();
   sf::FloatRect calcViewport();
 
 
 private:
+  sf::Clock imguiClock;
+  float fontScale = 2;
+
   // lots of DI (this is probably bad)
   sf::RenderWindow& window;
   Algorithms& algo;
   Graph& graph;
   LinkedList& linkedList;
-  float fontScale = 2;
+  Control& control;
+  sf::View& view;
 };
--- Sorting Algorithms/drawimgui.cpp
+++ Sorting Algorithms/drawimgui.cpp
@@ -2,8 +2,26 @@
 #include "DrawImgui.h"
 
 void DrawImgui::run() {
+  ImGuiIO& io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable docking
+  ImGui::SFML::Update(window, imguiClock.restart());
+
   dockspace();
   //ImGui::ShowDemoWindow();
+
+  ImGui::Begin("Visualizer");
+  ImGui::SetWindowFontScale(fontScale);
+
+  // scales viewport with gui width
+  view.setViewport(calcViewport());
+
+  if (ImGui::BeginTabBar("Tab Bar")) {
+    sortingAlgsTab();
+    linkedListTab();
+    ImGui::EndTabBar();
+  }
+
+  ImGui::End(); // End GUI Block
 }
 
 void DrawImgui::dockspace() {
@@ -20,6 +38,270 @@ void DrawImgui::dockspace() {
   ImGui::Begin("DockSpace", nullptr, window_flags);
   ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
   ImGui::End();
+}
+
+void DrawImgui::sortingAlgsTab() {
+  if (ImGui::BeginTabItem("Algorithms")) {
+    control.isAlgorithm = true;
+    control.isLinkedList = false;
+
+    // TODO: disable the button somehow when sorting
+    if (ImGui::Button(!control.isBubble ? "Bubble Sort" : "In Progress...")) {
+      if (!control.isSorting) {
+        algo.reset();
+        control.isSorting = true;
+        control.isBubble = true;
+        cout << "Bubble Sort... ";
+      }
+    }
+
+    if (ImGui::Button(!control.isSelection ? "Selection Sort" : "In Progress...")) {
+      if (!control.isSorting) {
+        algo.reset();
+        control.isSorting = true;
+        control.isSelection = true;
+        cout << "Selection Sort... ";
+      }
+    }
+
+    if (ImGui::Button(!control.isInsertion ? "Insertion Sort" : "In Progress...")) {
+      if (!control.isInsertion) {
+        algo.reset();
+        control.isSorting = true;
+        control.isInsertion = true;
+        cout << "Insertion Sort... ";
+      }
+    }
+
+    if (control.isSorting)
+      ImGui::BeginDisabled();
+    // TODO: implement skip animation
+    ImGui::Button("Skip (WIP)");
+    if (ImGui::Button("Shuffle")) {
+      graph.shuffle();
+    }
+    int prev = control.numBars;
+    ImGui::SliderInt("# of Bars", &control.numBars, 2, 100);
+    if (prev != control.numBars) {
+      graph.build(control.numBars);
+      prev = control.numBars;
+    }
+    if (control.isSorting)
+      ImGui::EndDisabled();
+
+    if (ImGui::Button(control.isPaused ? "Play" : "Pause")) {
+      control.isPaused = !control.isPaused;
+      algo.delayClock.restart();
+    }
+    // TODO: properly reset algorithm when in the middle of swapping
+    if (ImGui::Button("Stop (WIP)")) {
+      control.isSorting = false;
+      control.isPaused = false;
+      algo.reset();
+    }
+    // TODO: insert fancy graph of algorithm data
+    // TODO: have a popout window that shows the graph in an array form
+    ImGui::SliderFloat("Bar Speed", &control.speedMult, 0.1f, 100.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
+    ImGui::SliderFloat("Pause Timer", &DELAY, 0.f, 0.5f);
+
+    // probably use some var to handle the status message
+    if (!control.isSorting && !control.isPaused)
+      ImGui::Text("Status: IDLE");
+    else if (control.isSorting && !control.isPaused)
+      ImGui::Text("Status: RUNNING");
+    else if (control.isSorting && control.isPaused)
+      ImGui::Text("Status: PAUSED");
+
+    // %s doesn't take std::string, but rather char ptrs aka char[size]
+    const char* curState(algo.displayState[algo.getState()].c_str());
+    ImGui::Text("STATE: %s", curState);
+
+    // TODO: include min compares as well?
+    ImGui::TextWrapped("# of comparisons: %d", algo.mCompares);
+    if (control.isBubble)
+      control.bubbleCompares = max(algo.mCompares, control.bubbleCompares);
+    else if (control.isSelection)
+      control.selectionCompares = max(algo.mCompares, control.selectionCompares);
+    else if (control.isInsertion)
+      control.insertionCompares = max(algo.mCompares, control.insertionCompares);
+    ImGui::TextWrapped("Bubble Sort MAX Compares: %d", control.bubbleCompares);
+    ImGui::TextWrapped("Selection Sort MAX Compares: %d", control.selectionCompares);
+    ImGui::TextWrapped("Insertion Sort MAX Compares: %d", control.insertionCompares);
+
+    // sorting algorithms
+    if (control.isSorting && control.isBubble && !control.isPaused) {
+      if (algo.bubbleSort(graph)) {
+        control.isSorting = false;
+        control.isBubble = false;
+        algo.reset();
+      }
+    }
+
+    if (control.isSorting && control.isSelection && !control.isPaused) {
+      if (algo.selectionSort(graph)) {
+        control.isSorting = false;
+        control.isSelection = false;
+        algo.reset();
+      }
+    }
+
+    if (control.isSorting && control.isInsertion && !control.isPaused) {
+      if (algo.insertionSort(graph)) {
+        control.isSorting = false;
+        control.isInsertion = false;
+        algo.reset();
+      }
+    }
+
+    //algo.quickSort(graph, 0, graph.m_histogram.size() - 1);
+
+    ImGui::EndTabItem();
+  }
+}
+
+void DrawImgui::linkedListTab() {
+  if (ImGui::BeginTabItem("Linked Lists")) {
+    control.isAlgorithm = false;
+    control.isLinkedList = true;
+
+    if (ImGui::Button("Add")) {
+      linkedList.add();
+    }
+    if (ImGui::Button("Remove")) {
+      linkedList.remove();
+    }
+    if (ImGui::Button("Clear")) {
+      linkedList.clear();
+    }
+
+    // Mouse position scales with the view transformation (gui width)
+    // TODO: properly handle ptr when a node gets deleted
+    ImVec4 green(0.0f, 1.0f, 0.0f, 1.0f), white(1.0f, 1.0f, 1.0f, 1.0f);
+    sf::Vector2f convertMPos = window.mapPixelToCoords(control.mpos, window.getView());
+    if (control.isDragging) {
+      if (!linkedList.move(control.mpos)) {
+        throw("idk how I'm moving nothing");
+      }
+    }
+    ImGui::TextColored(linkedList.mouseInBounds(control.mpos) ? green : white, "Mapped Mouse (x:%0.0f, y:%0.0f)", convertMPos.x, convertMPos.y);
+
+    ImGui::TextWrapped("pActive: %p", linkedList.mActive);
+
+    ImGui::Text("Find Value: %d", control.nodeNum);
+    if (ImGui::Button(control.isSearching ? "Searching..." : "Find"))
+      control.isSearching = true;
+    if (control.isSearching) {
+      if (linkedList.findValueAnimated(control.nodeNum))
+        control.isSearching = false;
+    }
+    else
+      linkedList.updateCursor(linkedList.mActive);
+
+    //ImGui::SliderFloat("Cursor Speed", &, 1.0f, 5.0f);
+    ImGui::SliderFloat("Pause Timer", &DELAY, 0.1f, 0.5f);
+    
+    ImGui::TextWrapped(
+      "This data structure is an actual linked list, not a simulated one\n"
+      "Left click: click to select the node\n"
+      "Right click: anywhere to de-select node\n"
+      "Middle click: select node first, then middle click on node to popup\n"
+      "Add: create node at tail or after active node\n"
+      "Remove: removes node at tail or the active node\n"
+      "Clear: removes all nodes\n"
+      "Set Value: select a node and set value to change node value\n"
+      "Find Value: select node and type a # to search for a node\n"
+    );
+
+    if (control.isPopup) {
+      ImGui::OpenPopup("Node_popup");
+      control.isPopup = false;
+    }
+    if (ImGui::BeginPopup("Node_popup")) {
+      ImGui::Text("I'm a popup!");
+      ImGui::InputInt("Set Node Value:", &control.nodeNum);
+      if (ImGui::Button("Set"))
+        linkedList.mActive->setText(control.nodeNum);
+      ImGui::EndPopup();
+    }
+
+    // Note: this is so fking scuffed
+    ImGui::Begin("Linked List Data");
+    std::vector<std::string> text;
+    if (control.isSearching)
+      text = linkedList.parseString(linkedList.mStatePtr);
+    else
+      text = linkedList.parseString(linkedList.mActive);
+
+    linkedList.transformText(text);
+
+    if (text.size() > 0) {
+      if (text.size() == 1) {
+        ImGui::Text("head->%s", text.at(0).c_str());
+        ImGui::SameLine(0, 0);
+        ImGui::Text("null");
+      }
+      else if (text.size() == 2) {
+        if (text.at(0).at(0) == 'f') {
+          text.at(0).erase(text.at(0).begin());
+          ImGui::Text("head->");
+          ImGui::SameLine(0, 0);
+          ImGui::TextColored(green, "%s", text.at(0).c_str());
+          ImGui::SameLine(0, 0);
+          ImGui::Text("->");
+          ImGui::SameLine(0, 0);
+          ImGui::Text("%s", text.at(1).c_str());
+          ImGui::SameLine(0, 0);
+          ImGui::Text("null");
+        }
+        else if (text.at(0).at(0) == 'b') {
+          text.at(0).erase(text.at(0).begin());
+          ImGui::Text("head->%s", text.at(1).c_str());
+          ImGui::SameLine(0, 0);
+          ImGui::TextColored(green, "%s", text.at(0).c_str());
+          ImGui::SameLine(0, 0);
+          ImGui::Text("->");
+          ImGui::SameLine(0, 0);
+          ImGui::Text("null");
+        }
+      }
+      else if (text.size() == 3) {
+        ImGui::Text("head->%s", text.at(0).c_str());
+        ImGui::SameLine(0, 0);
+        ImGui::TextColored(green, "%s", text.at(1).c_str());
+        ImGui::SameLine(0, 0);
+        ImGui::Text("->");
+        ImGui::SameLine(0, 0);
+        ImGui::Text("%s", text.at(2).c_str());
+        ImGui::SameLine(0, 0);
+        ImGui::Text("null");
+      }
+    }
+    ImGui::End();
+
+    linkedList.dtRestart();
+    ImGui::EndTabItem();
+  }
+}
+
+void DrawImgui::binaryTreeTab() {
+  if (ImGui::BeginTabItem("Binary Trees")) {
+    ImGui::Text("test1");
+    ImGui::EndTabItem();
+  }
+}
+
+void DrawImgui::graphTab() {
+  if (ImGui::BeginTabItem("Graphs")) {
+    ImGui::Text("test2");
+    ImGui::EndTabItem();
+  }
+}
+
+void DrawImgui::mazeTab() {
+  if (ImGui::BeginTabItem("Mazes")) {
+    ImGui::Text("test3");
+    ImGui::EndTabItem();
+  }
 }
 
 sf::FloatRect DrawImgui::calcViewport() {
